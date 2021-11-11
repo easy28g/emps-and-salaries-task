b98b9214cab0c073bf54391b15638c57359be116
easy28g <fj@tk$q]3v>
2021-11-12 00:00:23 +0600

    Initial commit

---
+++ src/main/java/kg/company/empsandsalariestask/EmpsAndSalariesTaskApplication.java
@@ -0,0 +1,13 @@
+package kg.company.empsandsalariestask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpsAndSalariesTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpsAndSalariesTaskApplication.class, args);
+	}
+
+}
